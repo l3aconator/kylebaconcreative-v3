df0e7bb0ff975a478aff72c086e7b45a5de2fd40
Kyle Bacon <hello@kylebaconcreative.com>
2019-01-05 11:44:17 -0800

    Configure form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -22,7 +22,7 @@ class Contact extends React.Component {
                             <h3>I would love to help consult, code, or design your next project!</h3>
                         </div>
                         <fieldset class="contact-form">
-                            <form data-netlify="true" method="post" data-netlify-honeypot="bot-field" action="/contact/thanks">
+                            <form data-netlify="true" method="POST" data-netlify-honeypot="bot-field" name="contact" action="/contact/thanks">
                                 <div class="form-row">
                                     <div class="grid flex-grid">
                                         <div class="col--width__six">
